181baabfff35ecca2090fa5386bc560e42f74ba5
Mimi L <lim.mimic@gmail.com>
2026-06-26 09:49:26 +0200

    dice_rolling_game challenge completed

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,4 +1,32 @@
 import random
 
-print(random.randint(1, 6))
-#choice = input('Roll the dice? (y/n): ').lower(
+roll_count = 0
+
+def roll():
+   dice1 = random.randint(1, 6)
+   dice2 = random.randint(1, 6)
+   print(f'You rolled a {dice1} and {dice2}.')
+
+while True:
+   choice = input('Roll the dice? (y/n): ').lower()
+   if choice == 'y':
+      try: 
+         roll_num = int(input('How many times you wish to roll (between 1 and 10): '))
+         type(roll_num)
+         if isinstance(roll_num, int) and roll_num <= 10 and roll_num > 0:
+            while roll_num > 0:
+               roll()
+               roll_count = roll_count + 1
+               roll_num = roll_num - 1
+      except ValueError:
+         print('Invalid choice! Rolling once.')
+         roll()
+
+      
+   elif choice == 'n':
+      if roll_count > 0:
+         print(f'You have rolled the dices {roll_count} times.')
+      print('Thanks for playing. Bye!')
+      break
+   else:
+      print('Invalid choice.')
